b6370a09b41ac2d4f76e0f97adf7c962c718f6db
prnavpradep <prnavpradeepe@gmail.com>
2024-11-14 15:35:17 +0530

    mainiageee

--- src/components/ui/carousel/slider.tsx
+++ src/components/ui/carousel/slider.tsx
@@ -3,4 +3,4 @@
 import 'swiper/css';
 export { Navigation, Thumbs, Pagination, Autoplay, Grid } from 'swiper/modules';
 export { Swiper, SwiperSlide } from 'swiper/react';
-export type { SwiperOptions } from 'swiper';
+export type { SwiperOptions } from 'swiper/types';
